e244482b24e433070ad73f9f403f56483776661d
Kiran Muppana <76441268+sairamkiran9@users.noreply.github.com>
2025-06-15 10:40:47 -0400

    Updated firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,7 +1,10 @@
+// firebase.ts
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAnZxfRbpjzUFbv9OXRZonNm7mm9_S0Dgg",
   authDomain: "simplesuppers-68904.firebaseapp.com",
@@ -11,7 +14,16 @@ const firebaseConfig = {
   appId: "1:948449411635:web:d66b69669e4f3602235946",
 };
 
+// Initialize app
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+// Use initializeAuth for React Native
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+// Firestore
+const db = getFirestore(app);
+
+export { auth, db };
 export default app;
