25c9f251f920208e2367e907e691fededa450e54
Kiran Muppana <76441268+sairamkiran9@users.noreply.github.com>
2025-06-15 11:53:26 -0400

    Updated firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,6 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth/react-native';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from '@firebase/app';
+import { initializeAuth, getReactNativePersistence } from '@firebase/auth/';
+import { getFirestore } from '@firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
