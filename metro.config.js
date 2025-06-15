52d2faf13e4869f1f9847879c1bf70e0e9427127
Kiran Muppana <76441268+sairamkiran9@users.noreply.github.com>
2025-06-15 11:46:12 -0400

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,8 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 
 defaultConfig.resolver.sourceExts.push('cjs');
 defaultConfig.resolver.unstable_enablePackageExports = false;
 
-module.exports = config;
+module.exports = defaultConfig;
