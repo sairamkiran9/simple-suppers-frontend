237b85b45d7080a0109f160baecbb3dbae5b2341
Kiran Muppana <76441268+sairamkiran9@users.noreply.github.com>
2025-06-15 10:54:18 -0400

    Updated auth.tsx

---
+++ app/auth.tsx
@@ -0,0 +1,11 @@
+// screens/AuthScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AuthScreen = () => (
+  <View>
+    <Text>Welcome to Auth Screen</Text>
+  </View>
+);
+
+export default AuthScreen;
